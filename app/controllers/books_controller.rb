25662cfcaa95453eb2be678c200feb61033b4fe6
yuisato9688@gmail.com <ec2-user@ip-172-31-60-198.ec2.internal>
2021-06-16 11:02:57 +0000

    [4-10]ヘッダー作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,16 +1,34 @@
 class BooksController < ApplicationController
+
   def new
+    @book = Book.new
   end
 
   def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to book_path(@book)
   end
 
   def index
+    @book = Book.new
+    @books = Book.all
   end
 
   def show
+    @book =Book.find(params[:id])
   end
 
   def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to book_path
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
