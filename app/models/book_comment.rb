18855fe48d69252c44f7ee7162412ec841c5784f
waiwai888 <yuisato9688@gmail.com>
2021-07-01 15:45:38 +0000

    [finish]応用３

--- app/models/book_comment.rb
+++ app/models/book_comment.rb
@@ -1,9 +1,6 @@
 class BookComment < ApplicationRecord
   belongs_to :user
   belongs_to :book
-  def favorited_by?(user)
-    favorites.where(user_id: user.id).exists?
-  end
 
   validates :comment, presence: true
 end
