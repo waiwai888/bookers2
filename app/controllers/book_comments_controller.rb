99f038af8a9038c58eb0be104226fc7552086595
waiwai888 <yuisato9688@gmail.com>
2021-07-01 14:44:20 +0000

    [応用３]コメントバリデーション設定

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,11 +1,18 @@
 class BookCommentsController < ApplicationController
 
+
   def create
-    book = Book.find(params[:book_id])
-    comment = current_user.book_comments.new(book_comment_params)
-    comment.book_id = book.id
-    comment.save
-    redirect_to book_path(book)
+    @book = Book.find(params[:book_id])
+    @book_comment = current_user.book_comments.new(book_comment_params)
+    @book_comment.book_id = @book.id
+    if @book_comment.save
+      redirect_to book_path(@book.id)
+    else
+      @newbook = Book.new
+      @user = @book.user
+      @book_comments = @book.book_comments.all
+      render template: "books/show"
+    end
   end
 
   def destroy
