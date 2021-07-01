00785927bbbeca194c7cc802dac1264382ae18ea
waiwai888 <yuisato9688@gmail.com>
2021-07-01 14:57:12 +0000

    [応用３]いいね機能追加開始

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   get 'home/about' => 'homes#index'
   resources :books, only: [:create, :index, :show, :edit, :update, :destroy] do
     resources :book_comments, only: [:create, :destroy]
+    resource :favorites, only: [:create, :destroy]
   end
   resources :users, only: [:index, :show, :edit, :update]
 end
