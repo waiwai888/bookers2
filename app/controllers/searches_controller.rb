6957d1be8e2fdaa0d09d28c1ba39a6966c7bb8fa
waiwai888 <yuisato9688@gmail.com>
2021-07-05 02:53:43 +0000

    [finish]応用５　検索ワード上部に表示

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -3,12 +3,12 @@ class SearchesController < ApplicationController
   def search
     @range = params[:range]
     search = params[:search]
-    word = params[:word]
+    @word = params[:word]
 
     if @range == '1'
-      @user = User.search(search,word)
+      @user = User.search(search,@word)
     else
-      @book = Book.search(search,word)
+      @book = Book.search(search,@word)
     end
   end
 end
