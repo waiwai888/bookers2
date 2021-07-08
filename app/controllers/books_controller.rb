de4a5adce11caca770e046be8babf692ff0844ec
waiwai888 <yuisato9688@gmail.com>
2021-07-08 03:11:49 +0000

    [finish]応用７　8a　dm機能完成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -48,6 +48,25 @@ class BooksController < ApplicationController
     @user = @book.user
     @book_comment = BookComment.new
     @book_comments = @book.book_comments
+
+    @currentUserEntry=Entry.where(user_id: current_user.id)
+    @userEntry=Entry.where(user_id: @user.id)
+    if @user.id == current_user.id
+    else
+      @currentUserEntry.each do |cu|
+        @userEntry.each do |u|
+          if cu.room_id == u.room_id then
+            @isRoom = true
+            @roomId = cu.room_id
+          end
+        end
+      end
+      if @isRoom
+      else
+        @room = Room.new
+        @entry = Entry.new
+      end
+    end
   end
 
   def edit
--- app/models/room.rb
+++ app/models/room.rb
@@ -1,4 +1,7 @@
 class Room < ApplicationRecord
   has_many :messages, dependent: :destroy
   has_many :entries, dependent: :destroy
+
+  validates :content, presence: true,
+                      length: { maximum: 140}
 end
