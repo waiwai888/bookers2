cb7ec9069e88b48b10d9dc74e6b1da6ea453d05b
waiwai888 <yuisato9688@gmail.com>
2021-07-14 06:50:58 +0000

    [finish]7a復習　解答と別方法

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -14,18 +14,11 @@ class BooksController < ApplicationController
       render :index
       flash[:error] = "error"
     end
-
   end
 
   def index
     @book = Book.new
-    to  = Time.current.at_end_of_day
-    from  = (to - 6.day).at_beginning_of_day
-    @books = Book.includes(:favorited_users).
-      sort {|a,b|
-        b.favorited_users.includes(:favorites).where(created_at: from...to).size <=>
-        a.favorited_users.includes(:favorites).where(created_at: from...to).size
-      }
+    @books = Book.find(Favorite.group(:book_id).where(created_at: Time.current.all_week).order('count(book_id) desc').pluck(:book_id))
   end
 
   def show
