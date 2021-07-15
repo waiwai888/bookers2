deb9624fee28191f570e66e4cf0bdf37ba4b7b65
waiwai888 <yuisato9688@gmail.com>
2021-07-15 01:33:08 +0000

    7b投稿数表示完成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -27,6 +27,12 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @book = Book.new
     @books = Book.where(user_id: @user.id)
+    this_day = Date.today
+    @tdbooks = Book.where(user_id: @user.id, created_at: Date.today.beginning_of_day..Date.today.end_of_day)
+    @ysbooks = Book.where(user_id: @user.id, created_at: Date.yesterday.beginning_of_day..Date.yesterday.end_of_day)
+    @this_books = Book.where(user_id: @user.id, created_at: (this_day - (this_day.wday - 6) - 7).beginning_of_day..(this_day - (this_day.wday - 5)).end_of_day)
+    @last_books = Book.where(user_id: @user.id, created_at: ((this_day - (this_day.wday - 6) - 7)-7).beginning_of_day..((this_day - (this_day.wday - 5))- 7 ).end_of_day)
+
 
   end
 
